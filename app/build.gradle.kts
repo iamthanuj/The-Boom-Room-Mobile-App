03d952fd8eb1ac0d968a76acdac7e42cf8b1886e
iamthanuj <thanujdevil@gmail.com>
2023-11-15 03:18:29 +0530

    Creating starting page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.thebloomroom"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.thebloomroom"
---
+++ app/src/main/java/com/example/thebloomroom/StartingPageActivity.java
@@ -0,0 +1,20 @@
+package com.example.thebloomroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class StartingPageActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_page);
+
+
+
+    }
+}
