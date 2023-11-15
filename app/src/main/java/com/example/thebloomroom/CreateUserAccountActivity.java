4a8e06fa379efd9b9aaf715a49c04c062eb1b855
iamthanuj <thanujdevil@gmail.com>
2023-11-15 16:28:55 +0530

    login activity created

---
+++ app/src/main/java/com/example/thebloomroom/CreateUserAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.thebloomroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateUserAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user_account);
+    }
+}
--- app/src/main/java/com/example/thebloomroom/StartingPageActivity.java
+++ app/src/main/java/com/example/thebloomroom/StartingPageActivity.java
@@ -2,7 +2,9 @@ package com.example.thebloomroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class StartingPageActivity extends AppCompatActivity {
@@ -14,6 +16,14 @@ public class StartingPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_page);
 
+        button = findViewById(R.id.typeCusBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cusLoginMenuActivity = new Intent(getApplicationContext(),UserLoginActivity.class);
+                startActivity(cusLoginMenuActivity);
+            }
+        });
 
 
     }
