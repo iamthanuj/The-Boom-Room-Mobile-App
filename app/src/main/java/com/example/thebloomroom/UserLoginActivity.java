3c2ffe2efd54ca8a4566139c63a8855e53643532
iamthanuj <thanujdevil@gmail.com>
2023-11-15 23:24:23 +0530

    register activity created

--- app/src/main/java/com/example/thebloomroom/UserLoginActivity.java
+++ app/src/main/java/com/example/thebloomroom/UserLoginActivity.java
@@ -2,13 +2,27 @@ package com.example.thebloomroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserLoginActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        button = findViewById(R.id.createAccBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regScreenActivity = new Intent(getApplicationContext(), CreateUserAccountActivity.class);
+                startActivity(regScreenActivity);
+            }
+        });
     }
 }
