381f856964fc72e1b7ba736d565d4e7fb6816a2f
iamthanuj <thanujdevil@gmail.com>
2023-11-18 11:35:50 +0530

    user main panel added

--- app/src/main/java/com/example/thebloomroom/UserLoginActivity.java
+++ app/src/main/java/com/example/thebloomroom/UserLoginActivity.java
@@ -3,26 +3,100 @@ package com.example.thebloomroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.mindrot.jbcrypt.BCrypt;
 
 public class UserLoginActivity extends AppCompatActivity {
 
-    Button button;
+    private Button createAccBtn, userLoginBtn;
+    private EditText loginUsername, loginPassword;
+    private DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
 
-        button = findViewById(R.id.createAccBtn);
-        button.setOnClickListener(new View.OnClickListener() {
+        dbHelper = new DatabaseHelper(this);
+
+        loginUsername = findViewById(R.id.loginUsername);
+        loginPassword = findViewById(R.id.loginUserPassword);
+        createAccBtn = findViewById(R.id.createAccBtn);
+        userLoginBtn = findViewById(R.id.userLoginBtn);
+
+
+
+        //intent to register user activity
+        createAccBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent regScreenActivity = new Intent(getApplicationContext(), CreateUserAccountActivity.class);
                 startActivity(regScreenActivity);
             }
         });
+
+
+
+        //login user
+        userLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = loginUsername.getText().toString();
+                String password = loginPassword.getText().toString();
+
+                //input validation
+                if(username.isEmpty() || password.isEmpty()){
+                    String msg = "Please fill in the empty fields";
+                    Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+
+                } else if (checkUserCredentials(username, password)) {
+                    String msg = "login Successfully!";
+                    Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(),MainUserPanel.class);
+                    startActivity(intent);
+                }
+                else{
+                    String msg = "login Failed!";
+                    Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+
+
     }
+
+    private boolean checkUserCredentials(String username, String enteredPassword){
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] projection = {"password"};
+        String selection = "email = ?";
+        String[] selectionArgs = {username};
+
+        Cursor cursor =db.query("users", projection, selection, selectionArgs, null, null, null);
+
+        boolean credentialMatch = false;
+
+        if(cursor.getCount() > 0){
+            cursor.moveToFirst();
+            int passwordIndex = cursor.getColumnIndexOrThrow("password");
+            String storedPassword = cursor.getString(passwordIndex);
+
+            if(BCrypt.checkpw(enteredPassword, storedPassword)){
+                credentialMatch = true;
+            }
+
+        }
+        return credentialMatch;
+    }
+
 }
